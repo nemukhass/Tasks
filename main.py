c5e8176b2ab6ee0a26c819ac4e0d242ab1b2d80b
Mukhammadamin Nuriddinov <kingvb84@gmail.com>
2024-06-02 19:06:56 +0500

    4

--- main.py
+++ main.py
@@ -3,7 +3,11 @@
 # a = 46
 # b = 6
 # print(a,b*5)
-a = (5,"F","Привет",90.2)
-b = ("67")
-frozenset(b)
-print(a[2])
+# a = (5,"F","Привет",90.2)
+# b = ("67")
+# frozenset(b)
+# print(a[2])
+a = (input("vvedite 4 znacnoe chislo"))
+b = list(a)
+for i in b:
+    print(i)
